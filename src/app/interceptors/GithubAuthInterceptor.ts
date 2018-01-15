2708a9f137cebd4ee095f34936de1189df80d834
Volkov Sergey <truefunrun@hotmail.com>
2018-01-16 00:44:46 +0300

    remove oAuth github token

--- src/app/interceptors/GithubAuthInterceptor.ts
+++ src/app/interceptors/GithubAuthInterceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs/observable';
 export class GithubAuthInterceptor implements HttpInterceptor {
   intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authReq = req.clone({
-      headers: req.headers.set('Authorization', 'token 51d25e6961fd3a31a45e5046292e2355c200700a')
+      // headers: req.headers.set('Authorization', 'token 51d25e6961fd3a31a45e5046292e2355c200700a')
     });
     return next.handle(authReq);
   }
